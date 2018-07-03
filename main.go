3e745763e0cda1b82692d88349cfcb4c8197fa4d
Jakob Lindskog <jakob.lindskog@gmail.com>
2018-07-03 21:08:45 +0200

    add GiB support, clean up

--- main.go
+++ main.go
@@ -10,22 +10,23 @@ import (
 	"time"
 )
 
-type el struct {
+type pkg struct {
 	name       string
 	size       float64
 	sizeString string
 	date       time.Time
-	// compare    func(other el) float64
-	timeSort bool
+	timeSort   bool
 }
 
-func create(name string, size string, date string, timeSort bool) el {
+func create(name string, size string, date string, timeSort bool) pkg {
 
 	multiplier := 1e0
 	if strings.Contains(size, "MiB") {
 		multiplier = 1e6
 	} else if strings.Contains(size, "KiB") {
 		multiplier = 1e3
+	} else if strings.Contains(size, "GiB") {
+		multiplier = 1e9
 	}
 	val, _ := strconv.ParseFloat(strings.Trim(size[0:len(size)-3], " "), 64)
 	val *= multiplier
@@ -34,28 +35,26 @@ func create(name string, size string, date string, timeSort bool) el {
 	//Fri 20 Apr 2018 01:17:49 PM CEST
 	d, _ := time.Parse("Mon 02 Jan 2006 15:04:05 PM CEST", strings.Trim(date, " "))
 
-	el := el{strings.Trim(name, " "), val, strings.Trim(size, " "), d, timeSort}
+	el := pkg{strings.Trim(name, " "), val, strings.Trim(size, " "), d, timeSort}
 
 	return el
 }
 
-func (e *el) DateComp(other el) float64 {
+func (e *pkg) DateComp(other pkg) float64 {
 	return (float64)(-e.date.Unix() + other.date.Unix())
 }
 
-func (e *el) SizeComp(other el) float64 {
+func (e *pkg) SizeComp(other pkg) float64 {
 	return -e.size + other.size
 }
 
-type arr []el
+type arr []pkg
 
 func (a arr) Len() int {
 	return len(a)
 }
 
 func (a arr) Less(i, j int) bool {
-	// return a[i].SizeComp(a[j]) < 0
-	// return a[i].compare(a[j]) < 0
 	if a[i].timeSort {
 		return a[i].DateComp(a[j]) < 0
 	}
@@ -70,8 +69,9 @@ func (a arr) Swap(i, j int) {
 
 func main() {
 	flag.Usage = func() {
-
+		fmt.Println("Usage:\tlist-pkg [flags]")
 	}
+
 	t := flag.Bool("t", false, "Sort programs by install/update time instead of size (default)")
 	flag.Parse()
 
@@ -81,10 +81,10 @@ func main() {
 	}
 
 	fmt.Println(fmtPrint)
-	str, _ := exec.Command("sh", "-c", "pacman -Qei | grep -E 'Install Date|Name|Installed Size' | cut -d : -f 2,3,4 ").Output()
+	str, _ := exec.Command("sh", "-c", "yay -Qei | grep -E 'Install Date|Name|Installed Size' | cut -d : -f 2,3,4 ").Output()
 	Arr := strings.Split((string)(str), "\n")
 
-	aarr := (arr)(make([]el, len(Arr)/3))
+	aarr := (arr)(make([]pkg, len(Arr)/3))
 
 	for i := 0; i < len(Arr)-2; i += 3 {
 		a := create(Arr[i], Arr[i+1], Arr[i+2], *t)
